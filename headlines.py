51c6b738e95df0d86a17d4fec7316da06854f0f6
blvck <papaacquah17@gmail.com>
2017-07-21 17:20:14 +0000

    added currency rate feature

--- headlines.py
+++ headlines.py
@@ -13,7 +13,8 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 'fox':'http://feeds.foxnews.com/foxnews/latest',
 'iol':'http://www.iol.co.za/cmlink/1.640'}
 
-DEFAULTS = {'publication':'bbc','city':'London,UK'}
+DEFAULTS = {'publication':'bbc','city':'London,UK','currency_from':'GBP',
+            'currency_to':'USD'}
 
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
@@ -35,6 +36,13 @@ def get_weather(query):
                    "city":parsed["name"],"country":parsed["sys"]["country"]}
     return weather
 
+def get_rate(frm, to):
+    CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=42f428d203e4415ba9a51728ab57b83a"
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate / frm_rate, parsed.keys())
 
 
 @app.route("/")
@@ -45,7 +53,15 @@ def home():
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("home.html", articles = articles,weather=weather)
+    currency_from = request.args.get('currency_from')
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get('currency_to')
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html", articles = articles,weather=weather, currency_from = currency_from,
+                           currency_to = currency_to, rate = rate, currencies = sorted(currencies))
 
 
 if __name__ == "__main__":
